c136ce44fdfc7b91171585d9a0b84be8833c4f2c
Rohan Nethala <97348344+RohanNethala@users.noreply.github.com>
2025-09-30 15:13:54 -0400

    Updated server.c - works now!

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+// ...existing code...
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,211 +8,220 @@
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <errno.h>
+#include <signal.h>
 
-#define MAX_CLIENTS 5      /* accept up to 5 simultaneous clients as required */
-#define BUFFER_SIZE 1024
-#define PORT 12345
-
-/* Message board implementation moved here (was in message_board.c/h) */
+#define MAX_CLIENTS 5
 #define MAX_MESSAGES 10
-#define MAX_MESSAGE_LEN 1024
+#define MAX_MSG_LEN 1024
+#define LISTEN_BACKLOG 5
+#define SHUTDOWN_TOKEN "SERVER_SHUTDOWN\n"
 
-static char *board[MAX_MESSAGES];
-static int board_head = 0;   /* next write index */
-static int board_count = 0;  /* number of stored messages (<= MAX_MESSAGES) */
+static int client_sockets[MAX_CLIENTS];
+static int server_fd = -1;
 
-static void board_clear(void)
-{
-    for (int i = 0; i < MAX_MESSAGES; i++) {
-        free(board[i]);
-        board[i] = NULL;
-    }
-    board_head = 0;
-    board_count = 0;
+/* circular message board */
+static char messages[MAX_MESSAGES][MAX_MSG_LEN];
+static int msg_head = 0;
+static int msg_count = 0;
+
+static void board_clear(void) {
+    for (int i = 0; i < MAX_MESSAGES; i++)
+        messages[i][0] = '\0';
+    msg_head = 0;
+    msg_count = 0;
 }
 
-int post_message(const char *msg)
-{
-    if (!msg) return -1;
-    size_t len = strnlen(msg, MAX_MESSAGE_LEN);
-    char *copy = malloc(len + 1);
-    if (!copy) return -1;
-    memcpy(copy, msg, len);
-    copy[len] = '\0';
-
-    if (board[board_head]) {
-        free(board[board_head]);
-        board[board_head] = NULL;
-    }
-    board[board_head] = copy;
-    board_head = (board_head + 1) % MAX_MESSAGES;
-    if (board_count < MAX_MESSAGES) board_count++;
-    return 0;
+static void add_message(const char *msg) {
+    strncpy(messages[msg_head], msg, MAX_MSG_LEN-1);
+    messages[msg_head][MAX_MSG_LEN-1] = '\0';
+    msg_head = (msg_head + 1) % MAX_MESSAGES;
+    if (msg_count < MAX_MESSAGES) msg_count++;
 }
 
-char *get_board(void)
-{
-    size_t total = 1;
-    for (int i = 0; i < board_count; i++) {
-        int idx = (board_head - board_count + i + MAX_MESSAGES) % MAX_MESSAGES;
-        if (board[idx]) total += strlen(board[idx]) + 1;
-    }
-
-    char *res = malloc(total);
-    if (!res) return NULL;
-    res[0] = '\0';
-
-    for (int i = 0; i < board_count; i++) {
-        int idx = (board_head - board_count + i + MAX_MESSAGES) % MAX_MESSAGES;
-        if (board[idx]) {
-            strcat(res, board[idx]);
-        }
-        if (i < board_count - 1) strcat(res, "\n");
-    }
-    return res;
-}
-
-/* Minimal command handler (moved here). It writes responses to client_fd.
-   This keeps behaviour consistent with previous implementation. */
-void handle_command(int client_fd, const char *cmd)
-{
-    if (!cmd) return;
-
-    while (*cmd == ' ' || *cmd == '\t') cmd++;
-
-    if (strncmp(cmd, "POST ", 5) == 0) {
-        const char *msg = cmd + 5;
-        size_t mlen = strnlen(msg, MAX_MESSAGE_LEN);
-        char tmp[MAX_MESSAGE_LEN + 1];
-        if (mlen > MAX_MESSAGE_LEN) mlen = MAX_MESSAGE_LEN;
-        memcpy(tmp, msg, mlen);
-        tmp[mlen] = '\0';
-        while (mlen > 0 && (tmp[mlen-1] == '\n' || tmp[mlen-1] == '\r')) {
-            tmp[--mlen] = '\0';
-        }
-
-        if (post_message(tmp) == 0) {
-            const char ok[] = "OK\n";
-            send(client_fd, ok, sizeof(ok)-1, 0);
-        } else {
-            const char err[] = "ERROR: failed to post message\n";
-            send(client_fd, err, sizeof(err)-1, 0);
-        }
-    } else if (strncmp(cmd, "GET", 3) == 0 && (cmd[3] == '\0' || cmd[3] == '\n' || cmd[3] == '\r' || cmd[3] == ' ')) {
-        char *board_str = get_board();
-        if (board_str) {
-            size_t len = strlen(board_str);
-            if (len == 0) {
-                const char none[] = "\n";
-                send(client_fd, none, sizeof(none)-1, 0);
-            } else {
-                send(client_fd, board_str, len, 0);
-                const char nl[] = "\n";
-                send(client_fd, nl, 1, 0);
-            }
-            free(board_str);
-        } else {
-            const char err[] = "ERROR: unable to retrieve board\n";
-            send(client_fd, err, sizeof(err)-1, 0);
-        }
-    } else {
-        const char err[] = "ERROR: unknown command\n";
-        send(client_fd, err, sizeof(err)-1, 0);
+static void get_messages_string(char *out, size_t outlen) {
+    out[0] = '\0';
+    if (msg_count == 0) return;
+    int start = (msg_head - msg_count + MAX_MESSAGES) % MAX_MESSAGES;
+    for (int i = 0; i < msg_count; i++) {
+        int idx = (start + i) % MAX_MESSAGES;
+        strncat(out, messages[idx], outlen - strlen(out) - 1);
+        if (i < msg_count - 1) strncat(out, "\n", outlen - strlen(out) - 1);
     }
 }
 
-/* ...existing code... (main below) */
-int main() {
-    int server_fd, new_socket, max_sd, activity, i;
-    int client_sockets[MAX_CLIENTS] = {0};
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
+/* send msg to all clients except exclude_fd (if exclude_fd < 0 send to all) */
+static void broadcast_message(int exclude_fd, const char *msg, size_t len) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        int fd = client_sockets[i];
+        if (fd > 0 && fd != exclude_fd) {
+            ssize_t s = send(fd, msg, len, 0);
+            (void)s;
+        }
+    }
+}
 
-    fd_set readfds;
+/* SIGINT handler: notify clients, close sockets and exit */
+static void handle_sigint(int sig) {
+    /* print requested message when Ctrl+C (SIGINT) received */
+    printf("Shutting down server due to EOF.\n");
+    fflush(stdout);
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
+    const char *token = SHUTDOWN_TOKEN;
+    /* notify clients */
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] > 0) {
+            send(client_sockets[i], token, strlen(token), 0);
+            shutdown(client_sockets[i], SHUT_RDWR);
+            close(client_sockets[i]);
+            client_sockets[i] = 0;
+        }
+    }
+    if (server_fd >= 0) {
+        close(server_fd);
+        server_fd = -1;
+    }
+    board_clear();
+    _exit(0);
+}
+
+int main(int argc, char **argv) {
+    struct sockaddr_in servaddr, cliaddr;
+    socklen_t clilen;
+    fd_set rset, allset;
+    int i, maxi = -1, maxfd;
+    char buf[MAX_MSG_LEN];
+
+    const int BASE_PORT = 9877;
+    int offset = 0;
+    int port;
+
+    if (argc > 2) {
+        fprintf(stderr, "usage: %s [offset]\n", argv[0]);
+        exit(1);
+    }
+    if (argc == 2) {
+        offset = atoi(argv[1]);
+    }
+    /* if offset is 0 this yields BASE_PORT, otherwise BASE_PORT + offset */
+    port = BASE_PORT + offset;
+
+    for (i = 0; i < MAX_CLIENTS; i++) client_sockets[i] = 0;
+    board_clear();
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
     }
 
-    /* Allow reuse to avoid "address already in use" during rapid restarts */
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
+    if (bind(server_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind");
+        exit(1);
     }
 
-    if (listen(server_fd, 5) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
+    if (listen(server_fd, LISTEN_BACKLOG) < 0) {
+        perror("listen");
+        exit(1);
     }
 
-    printf("TCP server listening on port %d\n", PORT);
+    /* install SIGINT handler */
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_sigint;
+    sigaction(SIGINT, &sa, NULL);
 
-    while (1) {
-        FD_ZERO(&readfds);
-        FD_SET(server_fd, &readfds);
-        max_sd = server_fd;
+    FD_ZERO(&allset);
+    FD_SET(server_fd, &allset);
+    maxfd = server_fd;
 
-        for (i = 0; i < MAX_CLIENTS; i++) {
-            if (client_sockets[i] > 0) FD_SET(client_sockets[i], &readfds);
-            if (client_sockets[i] > max_sd) max_sd = client_sockets[i];
+    printf("TCP server listening on port %d\n", port);
+
+    for (;;) {
+        rset = allset;
+        int nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
+        if (nready < 0) {
+            if (errno == EINTR) continue;
+            perror("select");
+            break;
         }
 
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-
-        if ((activity < 0) && (errno != EINTR)) {
-            perror("Select error");
-        }
-
-        if (FD_ISSET(server_fd, &readfds)) {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-                perror("Accept error");
-                exit(EXIT_FAILURE);
+        if (FD_ISSET(server_fd, &rset)) {
+            clilen = sizeof(cliaddr);
+            int connfd = accept(server_fd, (struct sockaddr *)&cliaddr, &clilen);
+            if (connfd < 0) {
+                if (errno == EINTR) continue;
+                perror("accept");
+                continue;
             }
-
-            int free_index = -1;
+            int idx = -1;
             for (i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] == 0) {
-                    free_index = i;
-                    break;
-                }
+                if (client_sockets[i] == 0) { idx = i; break; }
             }
-
-            if (free_index == -1) {
-                printf("Too many clients, rejecting connection.\n");
-                close(new_socket);
+            if (idx == -1) {
+                const char *msg = "Too many clients\n";
+                send(connfd, msg, strlen(msg), 0);
+                close(connfd);
             } else {
-                client_sockets[free_index] = new_socket;
+                client_sockets[idx] = connfd;
+                FD_SET(connfd, &allset);
+                if (connfd > maxfd) maxfd = connfd;
+                if (idx > maxi) maxi = idx;
                 printf("New client connected.\n");
             }
+            if (--nready <= 0) continue;
         }
 
-        for (i = 0; i < MAX_CLIENTS; i++) {
-            int sd = client_sockets[i];
-            if (sd <= 0) continue;
-
-            if (FD_ISSET(sd, &readfds)) {
-                int valread = read(sd, buffer, BUFFER_SIZE);
-                if (valread == 0) {
-                    getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        for (i = 0; i <= maxi; i++) {
+            int sockfd = client_sockets[i];
+            if (sockfd <= 0) continue;
+            if (FD_ISSET(sockfd, &rset)) {
+                ssize_t n = recv(sockfd, buf, sizeof(buf)-1, 0);
+                if (n <= 0) {
+                    /* client closed */
                     printf("Client disconnected.\n");
-                    close(sd);
+                    close(sockfd);
+                    FD_CLR(sockfd, &allset);
                     client_sockets[i] = 0;
                 } else {
-                    buffer[valread] = '\0';
-                    handle_command(sd, buffer);
+                    buf[n] = '\0';
+                    /* strip trailing CR/LF */
+                    while (n > 0 && (buf[n-1] == '\n' || buf[n-1] == '\r')) { buf[n-1] = '\0'; n--; }
+
+                    if (strncmp(buf, "POST ", 5) == 0) {
+                        const char *msg = buf + 5;
+                        char msg_with_nl[MAX_MSG_LEN];
+                        snprintf(msg_with_nl, sizeof(msg_with_nl), "%s\n", msg);
+                        add_message(msg_with_nl);
+                        broadcast_message(sockfd, msg_with_nl, strlen(msg_with_nl));
+                    } else if (strcmp(buf, "GET") == 0) {
+                        char out[MAX_MESSAGES * (MAX_MSG_LEN + 1)];
+                        get_messages_string(out, sizeof(out));
+                        if (strlen(out) == 0) {
+                            const char *empty = "\n";
+                            send(sockfd, empty, 1, 0);
+                        } else {
+                            send(sockfd, out, strlen(out), 0);
+                            send(sockfd, "\n", 1, 0);
+                        }
+                    } else {
+                        const char *err = "ERROR: unknown command\n";
+                        send(sockfd, err, strlen(err), 0);
+                    }
                 }
+                if (--nready <= 0) break;
             }
         }
     }
 
+    /* cleanup */
+    handle_sigint(0);
     return 0;
 }
+// ...existing code...
