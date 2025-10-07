3633589bd4af888748953b5628675440f7e8ac5d
simonp2rpi <simonp2@rpi.edu>
2025-10-07 01:17:06 -0400

    updated main and worker_thread created

--- tcpcliserv/lab4.c
+++ tcpcliserv/lab4.c
@@ -1,4 +1,5 @@
 #include "unp.h"
+#include <pthread.h>
 
 typedef struct {
 int tid; // Thread index (1..T)
@@ -18,14 +19,35 @@ long fib(int n) {
     }
     return b;
 }
-int
-main(int argc, char **argv)
-{
+
+void *worker_thread(void *arg) {
+    fib_data_t *fib_data = (fib_data_t *)arg;
+    fib_data->sum = 0;
+    fib_data->result[0] = '\0';
+    char temp[32];
+
+    for (int i = fib_data->start; i <= fib_data->end; i++) {
+        long temp_fib = fib(i);
+        fib_data->sum += temp_fib;
+        snprintf(temp, sizeof(temp), "%ld ", temp_fib);
+        strncat(fib_data->result, temp);
+    }
+
+    printf("[Client %d][Thread %d] Range [%d–%d]: sum=%ld\n", /*something from client*/, fib_data->tid, fib_data->start, fib_data->end, fib_data->sum);
+
+    pthread_exit((void*)fib_data);
+}
+
+void *client_handler(void *arg) {
+    
+
+}
+
+int main(int argc, char **argv){
     if (argc != 3) err_quit("usage: ./lab4 port# #ofthreads");
-	int					listening_fd, connection_fd;
-	pid_t				childpid;
-	socklen_t			clilen;
-	struct sockaddr_in	cliaddr, servaddr;
+	int listening_fd, connection_fd;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
 
 	listening_fd = Socket(AF_INET, SOCK_STREAM, 0);
     int num_of_threads = atoi(argv[2]);
@@ -38,109 +60,15 @@ main(int argc, char **argv)
 	Bind(listening_fd, (SA *) &servaddr, sizeof(servaddr));
 
 	Listen(listening_fd, LISTENQ);
+    printf("[INFO] Server listening on port %s\n", argv[1]);
+
 
 	for ( ; ; ) {
 		clilen = sizeof(cliaddr);
 		connection_fd = Accept(listening_fd, (SA *) &cliaddr, &clilen);
+        printf("[INFO] New client from port %d\n", ntohs(cliaddr.sin_port));
 
-		// if ( (childtid = pthread_create()) == 0) {	/* child thread for client */
-		// 	Close(listening_fd);	/* close listening socket */
-		// 	str_echo(connection_fd);	/* process the request */
-		// 	exit(0);
-		// }
-
-		if ( (childpid = Fork()) == 0) {	/* child process */
-            Close(listening_fd);
-            int n = 0 ;
-            Write(connection_fd, "Enter N: ", 9);
-            char buffer[128];
-            ssize_t rn = Read(connection_fd, buffer, sizeof(buffer) - 1);
-            if (rn <= 0){
-                Close(connection_fd);
-                exit(0);
-            }
-            buffer[rn] = '\0';
-            int valid = sscanf(buffer, "%d", &n);
-            if (valid != 1 || n <= 0){
-                Write(connection_fd, "This number is invalid\n", 23);
-                Close(connection_fd);
-                exit(0);
-            }
-            // int i = 0;
-            int num_parallel_processes = n / num_of_threads;
-            int sums[num_of_threads];
-            int index = 0;
-            int count = 1;
-            for (int i = 0; i < num_of_threads; i++){
-                int sum = 0;
-                for (int u = 0; u < num_parallel_processes; u++){
-                    sum = sum + fib(count);
-                    count = count + 1;
-                }
-                sums[index] = sum;
-                index = index + 1;
-                // int fib_result = fib(count);
-                // fibs[count-1] = fib_result;
-                // count = count + 1;
-                // met = met + 1;
-                // if (met == num_parallel_processes){
-                //     met = 1;
-                //     int sum1 = 0;
-                //     for (int j = 0; j < num_parallel_processes; j++){
-                //         sum1 = sum1 + fibs[j];
-                //     }
-                //     sums[index] = sum1;
-                //     index = index + 1;
-                // }
-            }
-            // i = 0;
-            int final_sum = 0;
-            for (int i = 0; i < num_of_threads;i++){
-                final_sum = final_sum + sums[i];
-            }
-            printf("Final Sum: %d\n", final_sum);
-			Close(listening_fd);	/* close listening socket */
-			str_echo(connection_fd);	/* process the request */
-			exit(0);
-		}
-        // if (connection_fd > 0){
-        //     int n;
-        //     scanf("Enter N: %d\n", &n);
-        //     int i = 0;
-        //     int num_parallel_processes = n / num_of_threads;
-        //     int sums[num_of_threads];
-        //     int index = 0;
-        //     int count = 1;
-        //     for (i; i < num_of_threads; i++){
-        //         int sum = 0;
-        //         for (int u = 0; u < num_parallel_processes; u++){
-        //             sum = sum + fib(count);
-        //             count = count + 1;
-        //         }
-        //         sums[index] = sum;
-        //         index = index + 1;
-        //         // int fib_result = fib(count);
-        //         // fibs[count-1] = fib_result;
-        //         // count = count + 1;
-        //         // met = met + 1;
-        //         // if (met == num_parallel_processes){
-        //         //     met = 1;
-        //         //     int sum1 = 0;
-        //         //     for (int j = 0; j < num_parallel_processes; j++){
-        //         //         sum1 = sum1 + fibs[j];
-        //         //     }
-        //         //     sums[index] = sum1;
-        //         //     index = index + 1;
-        //         // }
-        //     }
-        //     i = 0;
-        //     int final_sum = 0;
-        //     for (i; i < num_of_threads;i++){
-        //         final_sum = final_sum + sums[i];
-        //     }
-        //     printf("Final Sum: %d\n", final_sum);
-
-        // }
-		Close(connection_fd);			/* parent closes connected socket */
+        pthread_t client_tid;
+        pthread_create(&client_tid, NULL, client_handler, (void *)&connection_fd);
 	}
 }
